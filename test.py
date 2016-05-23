79a8dff8ab0af7102a6b7180d0e23bd0e52dbb91
Rahul Raj <rahulraj1255@gmail.com>
2016-05-23 16:39:22 +0530

    Some changes

--- test.py
+++ test.py
@@ -5,8 +5,13 @@ import numpy as np
 import copy
 import math
 import sys
-
-x = pupildetect('5.jpg')
-cv2.imshow('final',x)
-cv2.waitKey(0)
+q=1
+cv2.namedWindow('sdf')
+for i in sys.argv:
+	if q==1:
+		q=0
+		continue
+	x = pupildetect(i)
+	cv2.imshow('sdf',x)
+	cv2.waitKey(3000)
 cv2.destroyAllWindows()
