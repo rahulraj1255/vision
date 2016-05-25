e2a3043309172838a65e2b10c6027f342da1f50e
rahulraj1255 <rahulraj1255@gmail.com>
2016-05-25 12:25:48 +0100

    added function encContour

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-from height import height
-from resize import resize
+from outer import encContour
+from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 import copy
 import math
 import sys
-<<<<<<< HEAD
 q=1
-cv2.namedWindow('sdf')
 for i in sys.argv:
 	if q==1:
 		q=0
 		continue
-	x = pupildetect(i)
-	cv2.imshow('sdf',x)
+	x = cv2.imread(i)
+	y=np.zeros(x.shape[0:2],dtype=np.uint8)
+	ct=encContour(i)
+	cv2.drawContours(y,[ct],0,(255),2)
+	plt.imshow(y,'gray')
+	plt.show()
 	cv2.waitKey(3000)
 cv2.destroyAllWindows()
-=======
-
-
-height('1.jpg',5)
->>>>>>> 3d7bec733d32263ded1fe80edcaf1b13fa0d3ef7
